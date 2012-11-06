a4d0e98400b79b2e95effce68573602dcbd95d43
victor <svcrobotics@gmail.com>
2012-11-06 23:10:15 +0100

    Initial commit

--- Quiz/QuizViewController.h
+++ Quiz/QuizViewController.h
@@ -9,5 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface QuizViewController : UIViewController
+{
+    int currentQuestionIndex;
+    
+    // The model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    // The view objects - don't worry about IBOutlet -
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
+
+// Methods
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
 
 @end
